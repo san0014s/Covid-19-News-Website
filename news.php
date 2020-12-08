54a8accad7377640e62eed9aa66d158c3f9925f3
lennon2jones <lj0039@mix.wvu.edu>
2020-12-08 11:02:06 -0500

    News Page Updated - Lennon Jones

--- news.php
+++ news.php
@@ -2,41 +2,44 @@
 	require 'includes/header.php'
 ?>
 <main>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link rel="stylesheet" href="css/news.css">
-	<div class="about-section">
-		<h1>Worldwide Coronavirus News</h1>
-	</div>
-	<div class="row">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/news.css">
+	<div class = "bg-cover">
+		<div class="jumbotron jumbotron-fluid">
+    		<h1 class="display-4">Global News</h1>
+    		<p class="lead">Global News of the COVID-19 Pandemic</p>
+		</div>
+		<div class="row">
 			<div class="card mx-auto">
-					<h2 class="title">Covid-19 Live Updates: Parts of Italy Will Lock Down Again as Europe Tries to Halt Second Wave</h2>
-					<p>The New York Times explores the impact of COVID-19 Worldwide</p>
-					<div class="center">
-						<p><a href="https://www.nytimes.com/live/2020/11/04/world/covid-19-coronavirus-updates" class="btn btn-dark btn-block active" role="button" aria-pressed="true">Read Now</a></p>
-					</div>
+				<h2 class="title">Covid-19 Live Updates: Parts of Italy Will Lock Down Again as Europe Tries to Halt Second Wave</h2>
+				<p>The New York Times explores the impact of COVID-19 Worldwide</p>
+				<div class="center">
+					<p><a href="https://www.nytimes.com/live/2020/11/04/world/covid-19-coronavirus-updates" class="btn btn-dark btn-block active" role="button" aria-pressed="true">Read Now</a></p>
+				</div>
 			</div>
 			<div class="card mx-auto">
 				<h2 class="title">McConnell calls for coronavirus package before end of the year</h2>
 				<h4>By: Marianne Levine</h4>	
 				<p>McConnell suggests that another COVID rescue package should be delivered</p>
-					<div class="center">
-						<p><a href="https://www.politico.com/news/2020/11/04/mcconnell-coronavirus-package-year-end-434047" class="btn btn-dark btn-block active" role="button" aria-pressed="true">Read Now</a></p>
-					</div>
+				<div class="center">
+					<p><a href="https://www.politico.com/news/2020/11/04/mcconnell-coronavirus-package-year-end-434047" class="btn btn-dark btn-block active" role="button" aria-pressed="true">Read Now</a></p>
+				</div>
         	</div>
-            <div class="card mx-auto">
-			<h2 class="title">Coronavirus: Why are infections rising again in US?</h2>
-					<p>This article describes how the U.S is handling COVID-19</p>
-					<div class="center">
-						<p><a href="https://www.bbc.com/news/election-us-2020-54423928" class="btn btn-dark btn-block active" role="button" aria-pressed="true">Read Now</a></p>
-					</div>
+        	<div class="card mx-auto">
+				<h2 class="title">Coronavirus: Why are infections rising again in US?</h2>
+				<p>This article describes how the U.S is handling COVID-19</p>
+				<div class="center">
+					<p><a href="https://www.bbc.com/news/election-us-2020-54423928" class="btn btn-dark btn-block active" role="button" aria-pressed="true">Read Now</a></p>
+				</div>
         	</div>
-            <div class="card mx-auto">
-			<h2 class="title">Face masks: what the data say</h2>
-					<h4>By: Lynne Peeples</h4>
-					<p>The science supports that face coverings are saving lives during the coronavirus pandemic, and yet the debate trundles on. How much evidence is enough?</p>
-					<div class="center">
-						<p><a href="https://www.nature.com/articles/d41586-020-02801-8" class="btn btn-dark btn-block active" role="button" aria-pressed="true">Read Now</a></p>
-					</div>
+        	<div class="card mx-auto">
+				<h2 class="title">Face masks: what the data say</h2>
+				<h4>By: Lynne Peeples</h4>
+				<p>The science supports that face coverings are saving lives during the coronavirus pandemic, and yet the debate trundles on. How much evidence is enough?</p>
+				<div class="center">
+					<p><a href="https://www.nature.com/articles/d41586-020-02801-8" class="btn btn-dark btn-block active" role="button" aria-pressed="true">Read Now</a></p>
+				</div>
         	</div>
-    </div>
+		</div>
+	</div>
 </main>
